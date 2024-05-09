dc3b6e776c2ab4813d68c08d7829e6dbd4ca85ff
UtsumiMinato <85283314+UtsumiMinato@users.noreply.github.com>
2024-05-09 10:49:34 +0800

    fix:修正FindSpecific內的回傳類型

--- app/src/main/java/Model/FindSpecific.java
+++ app/src/main/java/Model/FindSpecific.java
@@ -6,12 +6,16 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
 //Synchronous
-//user 輸入 keyword 後，使用keyword查詢database的table內相符合的路段。ex:使用者輸入大同路，SELECT ＊, FROM pork_spot, WHERE;
+//user 輸入 keyword 後，使用keyword查詢database的table內相符合的路段。ex:使用者輸入大同路後->SELECT ＊, FROM pork_spot, WHERE parkinglot_address LIKE ?;
 public class FindSpecific {
     String KeyWord;
-    public void querySpot(String KeyWord){
+    public Map querySpot(String KeyWord){
+        Map <Integer, String> parkingSpot = new HashMap<>();
+//        String parkingSpot = null; //還懶著想怎麼存，先string 測試先
         try {
             // 獲取資料庫連接
             Connection conn = DatabaseConnection.getConnection();
@@ -28,8 +32,8 @@ public class FindSpecific {
             while (rs.next()) {
                 int parkinglot_id = rs.getInt("parkinglot_id");
                 String parkinglot_name = rs.getString("parkinglot_name");
-                // 其他欄位類似
-                System.out.println("ID: " + parkinglot_id + ", Name: " + parkinglot_name);
+//                System.out.println("ID: " + parkinglot_id + ", Name: " + parkinglot_name);
+                parkingSpot.put(parkinglot_id, parkinglot_name);
             }
 
             // 關閉資源
@@ -39,5 +43,6 @@ public class FindSpecific {
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
+    return parkingSpot;
     }
 }
