e553c69aef8162f8b426b90c532da59ee6f14242
HuangYi-Chen <140423515+HuangYi-Chen@users.noreply.github.com>
2024-05-11 02:10:56 +0800

    activity_main_xml and search_view_bg.sml

--- app/src/main/java/com/example/parkingapp/MainActivity.java
+++ app/src/main/java/com/example/parkingapp/MainActivity.java
@@ -4,17 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 import Model.FindSpecific;
 
 public class MainActivity extends AppCompatActivity {
     private Button finBtn;
+    private SearchView searchview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        searchView = findViewById(R.id.searchView);
+        searchView.clearFocus();
+        searchView.setOnQueryTextListner(new SearchView.OnQueryTextListener(){
+            @Override
+            public boolean onQueryTextSubmit(String query){
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText){
+                filterList(newText);
+                return true;
+            }
+        });
         // 初始化按鈕
         finBtn = findViewById(R.id.findBtn);
 
@@ -27,5 +44,20 @@ public class MainActivity extends AppCompatActivity {
                 findSpecific.querySpot("南港路");
             }
         });
+    }
+
+    private void filterList(String Text){
+        List<Item> filteredlist = new ArrayList<>();
+        for (Item item : itemList){
+            if (item.getItemName().toLowerCase.contains(text.toLowerCase())){
+                filterList.add(item);
+            }
+        }
+
+        if (filteredlist.isEmpty()){
+            Toast.makeText(this, "No data found", Toast.LENGTH_SHORT).show();
+        }else{
+
+        }
     }
 }
