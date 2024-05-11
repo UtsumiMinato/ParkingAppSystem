d75ad9f01545d63051734f869acf6a4157764f9e
HuangYi-Chen <140423515+HuangYi-Chen@users.noreply.github.com>
2024-05-11 22:42:28 +0800

    改下面欄位和語音搜尋

--- app/src/main/java/com/example/parkingapp/MainActivity.java
+++ app/src/main/java/com/example/parkingapp/MainActivity.java
@@ -1,17 +1,24 @@
 package com.example.parkingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.context.Intext;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.speech.RecognitionListener;
+import android.speech.SpeechRecognizer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SearchView;
 import android.widget.Toast;
+import java.util.ArrayList;
+import java.util.List;
 
 import Model.FindSpecific;
 
 public class MainActivity extends AppCompatActivity {
     private Button finBtn;
     private SearchView searchview;
+    private List<Item>itemList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +30,14 @@ public class MainActivity extends AppCompatActivity {
         searchView.setOnQueryTextListner(new SearchView.OnQueryTextListener(){
             @Override
             public boolean onQueryTextSubmit(String query){
-                return false;
+                filterList(query);
+                return true;
             }
 
             @Override
             public boolean onQueryTextChange(String newText){
-                filterList(newText);
-                return true;
+
+                return false;
             }
         });
         // 初始化按鈕
@@ -58,6 +66,26 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "No data found", Toast.LENGTH_SHORT).show();
         }else{
 
+        }
+    }
+
+    //啟動語音輸入
+    private void startVoiceInput(){
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "請開始說話...");
+        startActivityForResult(intent,100);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 100 && resultCode == RESULT_OK && data != null){
+            ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            if(result != null && !result.isEmpty()){
+                String spokenText = result.get(0);
+                searchview.setQuery(spokenText, true);
+            }
         }
     }
 }
