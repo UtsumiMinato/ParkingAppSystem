9bc6090d498e52a3cc1773b65b3f19fe5e22c6bb
UtsumiMinato <newxmith@gmail.com>
2024-06-02 05:46:11 +0800

    feat:加入標記

--- app/src/main/java/com/example/parkingapp/MapsFragment.java
+++ app/src/main/java/com/example/parkingapp/MapsFragment.java
@@ -19,15 +19,19 @@ import androidx.appcompat.widget.SearchView;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.CameraPosition;
 
+
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import android.widget.Toast;
@@ -37,10 +41,20 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
     private GoogleMap mMap;
     private SearchView searchView;
     private FusedLocationProviderClient mLocationClient;
+    private List<LatLng> predefinedLocations;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        predefinedLocations = new ArrayList<>();
+        // Add predefined locations
+        predefinedLocations.add(new LatLng(23.69240510302933, 120.54115858531878));
+        predefinedLocations.add(new LatLng(23.69589083905699, 120.52695418898324));
+        predefinedLocations.add(new LatLng(23.697305531011292, 120.52502299857244));
+        predefinedLocations.add(new LatLng(23.697619904919158, 120.52751208843522));
+        predefinedLocations.add(new LatLng(23.701588810386625, 120.53150321528412));
+        predefinedLocations.add(new LatLng(23.699034578122333, 120.54163123632712));
+
         return inflater.inflate(R.layout.fragment_maps, container, false);
     }
 
@@ -72,82 +86,83 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mMap.getUiSettings().setScrollGesturesEnabled(true);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.getUiSettings().setZoomGesturesEnabled(true);
+        mMap.getUiSettings().setRotateGesturesEnabled(true);
         if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             mMap.setMyLocationEnabled(true);
             getDeviceLocation();
         } else {
-            // Set the map coordinates to Kyoto Japan.
-            LatLng kyoto = new LatLng(35.00116, 135.7681);
-            // Set the map type to Hybrid.
-            googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-            // Add a marker on the map coordinates.
-            googleMap.addMarker(new MarkerOptions()
-                    .position(kyoto)
-                    .title("Kyoto"));
-            // Move the camera to the map coordinates and zoom in closer.
-            googleMap.moveCamera(CameraUpdateFactory.newLatLng(kyoto));
-            googleMap.moveCamera(CameraUpdateFactory.zoomTo(15));
-            // Display traffic.
-            googleMap.setTrafficEnabled(true);
-
-            //設定預設北方朝上
-            CameraPosition cameraPosition = new CameraPosition.Builder()
-                    .target(kyoto)
-                    .zoom(15)
-                    .bearing(0)  // 北方
-                    .build();
-            googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+            mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
         }
     }
 
+    private void searchLocation(String query) {
+        if (query.equalsIgnoreCase("douliu")) {
+            mMap.clear(); // 清除地圖標記
+            if (!predefinedLocations.isEmpty()) {
+                LatLngBounds.Builder builder = new LatLngBounds.Builder();
+                for (LatLng location : predefinedLocations) {
+                    mMap.addMarker(new MarkerOptions().position(location).title("Predefined Location"));
+                    builder.include(location);
+                }
+                LatLngBounds bounds = builder.build();
+                CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngBounds(bounds, 100);
+                mMap.animateCamera(cameraUpdate);
+                CameraPosition cameraPosition = new CameraPosition.Builder()
+                        .target(bounds.getCenter())
+                        .zoom(mMap.getCameraPosition().zoom)
+                        .bearing(0) // 北方朝上
+                        .build();
+                mMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+            }
+        } else {
+            Geocoder geocoder = new Geocoder(getContext());
+            try {
+                List<Address> addresses = geocoder.getFromLocationName(query, 1);
+                if (!addresses.isEmpty()) {
+                    Address address = addresses.get(0);
+                    LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+                    mMap.clear(); // 清除之前的標記
+                    mMap.addMarker(new MarkerOptions().position(latLng).title(query));
+                    CameraPosition cameraPosition = new CameraPosition.Builder()
+                            .target(latLng)
+                            .zoom(15)
+                            .bearing(0) // 北方朝上
+                            .build();
+                    mMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+                } else {
+                    Toast.makeText(getContext(), "Location not found: " + query, Toast.LENGTH_LONG).show();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
     private void getDeviceLocation() {
         if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
             return;
         }
 
         mLocationClient.getLastLocation()
                 .addOnSuccessListener(location -> {
                     if (location != null) {
-                        double latitude = location.getLatitude();
-                        double longitude = location.getLongitude();
-                        Log.d("Location", "Current location: Latitude " + latitude + ", Longitude " + longitude);
-                        LatLng currentLocation = new LatLng(latitude, longitude);
-                        CameraPosition cameraPosition = new CameraPosition.Builder()
-                                .target(currentLocation)
-                                .zoom(15)
-                                .bearing(0)  //北方
-                                .build();
-                        mMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+                        LatLng currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 15));
                         mMap.addMarker(new MarkerOptions().position(currentLocation).title("Current Location"));
                     } else {
                         Log.d("Location", "No location retrieved");
+                        Toast.makeText(getContext(), "Current location not found", Toast.LENGTH_LONG).show();
                     }
                 })
                 .addOnFailureListener(e -> {
                     Log.d("Location", "Error trying to get last GPS location", e);
+                    Toast.makeText(getContext(), "Error trying to get current location", Toast.LENGTH_LONG).show();
                 });
     }
 
-    private void searchLocation(String location) {
-        Geocoder geocoder = new Geocoder(getContext());
-        try {
-            List<Address> addresses = geocoder.getFromLocationName(location, 1);
-            if (!addresses.isEmpty()) {
-                Address address = addresses.get(0);
-                LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
-                mMap.clear();
-                CameraPosition cameraPosition = new CameraPosition.Builder()
-                        .target(latLng)
-                        .zoom(12)
-                        .bearing(0)  //北方
-                        .build();
-                mMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-                mMap.addMarker(new MarkerOptions().position(latLng).title(location));
-            } else {
-                Log.d("Location", "Location not found: " + location);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 }
