1920bff48b6e647b49ccde87a4cbff575f7f6dd0
UtsumiMinato <newxmith@gmail.com>
2024-06-02 06:10:53 +0800

    feat:加入標點組合功能

--- app/src/main/java/com/example/parkingapp/MapsFragment.java
+++ app/src/main/java/com/example/parkingapp/MapsFragment.java
@@ -1,11 +1,7 @@
 package com.example.parkingapp;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.core.app.ActivityCompat;
-import androidx.fragment.app.Fragment;
-
 import android.Manifest;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
@@ -15,31 +11,36 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.Fragment;
 import androidx.appcompat.widget.SearchView;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.CameraPosition;
+import com.google.maps.android.clustering.ClusterManager;
+import com.google.android.gms.maps.model.LatLngBounds;
 
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import android.widget.Toast;
-
 public class MapsFragment extends Fragment implements OnMapReadyCallback {
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
     private GoogleMap mMap;
     private SearchView searchView;
+    private ClusterManager<MapClusterItem> mClusterManager;
     private FusedLocationProviderClient mLocationClient;
     private List<LatLng> predefinedLocations;
 
@@ -47,15 +48,17 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         predefinedLocations = new ArrayList<>();
-        // Add predefined locations
+        addPredefinedLocations();
+        return inflater.inflate(R.layout.fragment_maps, container, false);
+    }
+
+    private void addPredefinedLocations() {
         predefinedLocations.add(new LatLng(23.69240510302933, 120.54115858531878));
         predefinedLocations.add(new LatLng(23.69589083905699, 120.52695418898324));
         predefinedLocations.add(new LatLng(23.697305531011292, 120.52502299857244));
         predefinedLocations.add(new LatLng(23.697619904919158, 120.52751208843522));
         predefinedLocations.add(new LatLng(23.701588810386625, 120.53150321528412));
         predefinedLocations.add(new LatLng(23.699034578122333, 120.54163123632712));
-
-        return inflater.inflate(R.layout.fragment_maps, container, false);
     }
 
     @Override
@@ -63,7 +66,6 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
         super.onViewCreated(view, savedInstanceState);
         searchView = view.findViewById(R.id.idSearchView);
         mLocationClient = LocationServices.getFusedLocationProviderClient(getActivity());
-
         SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         if (mapFragment != null) {
             mapFragment.getMapAsync(this);
@@ -86,36 +88,62 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        setUpClusterer();
         mMap.getUiSettings().setScrollGesturesEnabled(true);
         mMap.getUiSettings().setZoomControlsEnabled(true);
         mMap.getUiSettings().setZoomGesturesEnabled(true);
         mMap.getUiSettings().setRotateGesturesEnabled(true);
-        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+        checkLocationPermission();
+    }
+
+    private void checkLocationPermission() {
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION)) {
+                new AlertDialog.Builder(getContext())
+                        .setTitle("Location Permission Needed")
+                        .setMessage("This app needs the Location permission, please accept to use location functionality")
+                        .setPositiveButton("OK", (dialogInterface, i) -> {
+                            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
+                        })
+                        .create()
+                        .show();
+            } else {
+                ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
+            }
+        } else {
             mMap.setMyLocationEnabled(true);
             getDeviceLocation();
-        } else {
-            mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        }
+    }
+
+    private void setUpClusterer() {
+        mClusterManager = new ClusterManager<>(getContext(), mMap);
+        mMap.setOnCameraIdleListener(mClusterManager);
+        mMap.setOnMarkerClickListener(mClusterManager);
+        addItems();
+    }
+
+    private void addItems() {
+        for (LatLng location : predefinedLocations) {
+            MapClusterItem item = new MapClusterItem(location, "Predefined Location", "Snippet");
+            mClusterManager.addItem(item);
         }
     }
 
     private void searchLocation(String query) {
-        if (query.equalsIgnoreCase("douliu")) {
-            mMap.clear(); // 清除地圖標記
+        if ("douliu".equalsIgnoreCase(query)) {
+            mClusterManager.clearItems(); // 清除所有 cluster items
+            addItems(); // 重新加速 all items
+            mClusterManager.cluster();
+
+            // 計算所有預定義位置的邊界以聚焦地圖
             if (!predefinedLocations.isEmpty()) {
                 LatLngBounds.Builder builder = new LatLngBounds.Builder();
                 for (LatLng location : predefinedLocations) {
-                    mMap.addMarker(new MarkerOptions().position(location).title("Predefined Location"));
                     builder.include(location);
                 }
                 LatLngBounds bounds = builder.build();
-                CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngBounds(bounds, 100);
-                mMap.animateCamera(cameraUpdate);
-                CameraPosition cameraPosition = new CameraPosition.Builder()
-                        .target(bounds.getCenter())
-                        .zoom(mMap.getCameraPosition().zoom)
-                        .bearing(0) // 北方朝上
-                        .build();
-                mMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+                mMap.animateCamera(CameraUpdateFactory.newLatLngBounds(bounds, 100));
             }
         } else {
             Geocoder geocoder = new Geocoder(getContext());
@@ -144,7 +172,6 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
 
     private void getDeviceLocation() {
         if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
             return;
         }
 
@@ -155,14 +182,27 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
                         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 15));
                         mMap.addMarker(new MarkerOptions().position(currentLocation).title("Current Location"));
                     } else {
-                        Log.d("Location", "No location retrieved");
                         Toast.makeText(getContext(), "Current location not found", Toast.LENGTH_LONG).show();
                     }
                 })
                 .addOnFailureListener(e -> {
-                    Log.d("Location", "Error trying to get last GPS location", e);
+                    Log.d("Location", "Error trying to get last GPS location");
                     Toast.makeText(getContext(), "Error trying to get current location", Toast.LENGTH_LONG).show();
                 });
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (mMap != null) {
+                    mMap.setMyLocationEnabled(true);
+                    getDeviceLocation();
+                }
+            } else {
+                Toast.makeText(getContext(), "Location permission denied.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 }
